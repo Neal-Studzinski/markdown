33d4d9d22553d5ee46c8a7f949ef5a6ce4b7c8cf
NEAL STUDZINSKI <nealstudzinski@gmail.com>
2017-05-01 21:22:01 -0500

    working

--- app/scripts/components/enter-post.js
+++ app/scripts/components/enter-post.js
@@ -8,10 +8,14 @@ class EnterPost extends React.Component {
     constructor(props) {
         super(props);
         this.enterText = this.enterText.bind(this);
+        this.submitMarkdownNotes = this.submitMarkdownNotes.bind(this);
+        this.dismissConfirmationMessage = this.dismissConfirmationMessage.bind(this);
+        this.confirmationMessage = this.confirmationMessage.bind(this);
     }
 
     enterText(e) {
         this.auto_grow(e.target);
+        
         this.props.dispatch({ type: 'UPDATE_DRAFT', rawText: e.target.value });
     }
 
@@ -24,11 +28,39 @@ class EnterPost extends React.Component {
         element.style.height = element.scrollHeight + 'px';
     }
 
+    submitMarkdownNotes() {
+        const draft = this.props.draft;
+
+
+        this.props.dispatch({
+            type: "SUBMIT_NOTES",
+
+        })
+        console.log('Notes sumbitted to Backendless')
+    }
+
+    dismissConfirmationMessage() {
+        this.props.dispatch({
+            type: 'DISMISS_CONFIRMATION'
+        });
+    }
+
+    confirmationMessage() {
+        if (this.props.showConfirmationMessage == true) {
+            return <div className ='confirmation-message'>You have submitted your note to the server
+                <button onClick={this.dismissConfirmationMessage} className = 'dismiss-button'>Dismiss
+                </button>
+                </div>
+        }else {
+            return '';
+        }
+    }
+
     render() {
         var marked = require('marked');
         return (
             <div className = 'post-editor'>
-                <h2>New Blog Post</h2>
+                <h2>New Note</h2>
                 <div className = 'editor-container'>
                     <section className = 'post-entry'>
                         <textarea
@@ -40,6 +72,11 @@ class EnterPost extends React.Component {
                             cols = '60'
                             placeholder = 'type post in markdown'
                         />
+                        <div className = 'submit-button-container'>
+                            <button className = 'submit-button' type = 'submit' onClick = {this.submitMarkdownNotes}>submit
+                            </button>
+                        </div>
+                        {this.confirmationMessage()}
                     </section>
                     <section className = 'markdown-preview-section'>
                         <article
--- app/scripts/reducers/app_reducer.js
+++ app/scripts/reducers/app_reducer.js
@@ -1,6 +1,9 @@
+import store from '../store.js';
+
+
 let initialState = {
-    savedPosts: [],
-    draft: ''
+    draft: '',
+    showConfirmationMessage: false
 };
 
 export default function AppReducer(currentState, action) {
@@ -13,10 +16,63 @@ export default function AppReducer(currentState, action) {
         var newState = {
             draft:action.rawText
         };
-      // DELETE OR CHANGE ME: I AM JUST AN EXAMPLE
-      return Object.assign({}, currentState, newState);
+        return Object.assign({}, currentState, newState);
+
+    case "SUBMIT_NOTES":
+        let newState = {
+            draft : currentState.draft
+
+        }
+        console.log('draft', newState.draft);
+        console.log('length', newState.draft.length);
+        if (newState.draft.length > 50) {
+            $.ajax({
+                type: 'POST',
+                url: 'https://api.backendless.com/68949845-B4E0-193D-FFA9-CDED0DACCC00/7765F30C-6FB7-1900-FFA4-4BB1A69E2000/data/Markdown',
+                headers: {
+                    // 'application-id': '68949845-B4E0-193D-FFA9-CDED0DACCC00',
+                    // 'secret-key': '7765F30C-6FB7-1900-FFA4-4BB1A69E2000',
+                    'application-type': 'REST',
+                    'Content-Type': 'application/json'
+                },
+                data: JSON.stringify({
+                    'Notes': newState.draft
+                }),
+                success: (data,status,xhr) => {
+                    store.dispatch({type: 'SHOW_CONFIRMATION'});
+                    store.dispatch({type: 'EMPTY_MARKDOWN_NOTES'});
+                }
+            })
+            return Object.assign({}, currentState, newState);
+
+        }else {
+            return currentState;
+        }
+
+    case 'EMPTY_MARKDOWN_NOTES':
+        var newState = {
+            draft : '',
+            markdownPreview : ''
+        };
+        return Object.assign({}, currentState, newState)
+
+    case 'DISMISS_CONFIRMATION':
+        var newState = {
+            showConfirmationMessage: false
+        }
+        return Object.assign({}, currentState, newState)
+
+    case 'SHOW_CONFIRMATION':
+        var newState = {
+            showConfirmationMessage: true
+        }
+        return Object.assign({}, currentState, newState)
+
+    default:
+        return currentState
+
   }
 
-  
-  return currenState;
+
+
 }
